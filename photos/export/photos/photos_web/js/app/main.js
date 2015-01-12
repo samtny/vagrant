59501e9fb8000be05b6f977cbf7f38ccb3bd03c0
Sam T <samtny@gmail.com>
2015-01-12 09:43:08 -0500

    remove export

--- photos/export/photos/photos_web/js/app/main.js
+++
@@ -1,5 +0,0 @@
-define(['./lib', 'knockout', './ViewModel/MainViewModel', 'underscore'], function (lib, ko, mainViewModel, _) {
-  ko.applyBindings(new mainViewModel({
-    host: 'https://ec2-54-165-129-228.compute-1.amazonaws.com'
-  }));
-});
